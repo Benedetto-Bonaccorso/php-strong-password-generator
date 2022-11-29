f3ae422bf4aaadcee2405efecc69b47258ac95a9
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-11-29 14:52:38 +0100

    milestone1

--- index.php
+++ index.php
@@ -23,7 +23,16 @@ Invece di visualizzare la password nella index, effettuare un redirect ad una pa
     echo $passwordLength;
 
     for ($i = 0; $i <= $passwordLength; $i++) {
-        $output .= $i;
+        $rng = rand(1,4);
+        if($rng == 1){
+            $output .= rand(0,9);
+        } else if($rng == 2){
+            $output .= chr(rand(65,90));
+        } else if($rng == 3){
+            $output .= $letter = chr(rand(97,122));
+        } else if($rng == 4){
+            $output .= chr(rand(33,38));;
+        }
       };
 
       echo "<hr>";
