e83ff11c629b8d9f88c4e6d6f04645c39edbe20e
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-11-29 14:29:01 +0100

    aggiunta indice a output

---
+++ index.php
@@ -0,0 +1,68 @@
+<!--
+    Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. Scriviamo tutto (logica e layout) in un unico file index.php
+
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+
+Milestone 3 (BONUS)
+Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli.
+Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme). Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali.
+
+Milestone 4 (BONUS - OPZIONALE)
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION (documentazione) recupererà la password da mostrare all’utente.
+-->
+<?php 
+
+    $output = "";
+    $passwordLength = $_GET["length"];
+    echo $passwordLength;
+
+    for ($i = 0; $i <= $passwordLength; $i++) {
+        $output .= $i;
+      };
+
+      echo "<hr>";
+      echo $output;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</head>
+<body>
+    <form action="index.php">
+
+        <p>Input desired length of Password</p><input type="text" name="length" id="length"></input>
+        
+        <div class="character-repetition d-flex justify-content-between">
+        <!--
+            <p>Allow repetition of characters?</p>
+        
+            <div class="d-flex flex-column">
+                <div class="radio-wrapper">
+                    <input type="radio" name="allowRepetition" id="allowRepetition" value="yes">
+                </div>
+
+                <div class="radio-wrapper">
+                    <input type="radio" name="allowRepetition" id="allowRepetition" value="no">
+                </div>
+            </div>
+        -->
+            </div>
+        <button type="submit">Submit</button>
+
+    </form>
+</body>
+</html>
